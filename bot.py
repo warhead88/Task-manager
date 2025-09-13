160a66dc88ee2ed2fdc60e4b173f563858ab1e1f
w88 <krippach147@gmail.com>
2025-09-13 18:18:30 +0500

    Updated .gitignore && added minimal functionality

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config import Config
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
+
+async def main():
+    bot = Bot(token=Config.BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(start.router)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())

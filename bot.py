0b41b576ee4397f41867c78dcd77e17090f9ece1
w88 <krippach147@gmail.com>
2025-09-14 21:02:00 +0500

    Added remove command

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 
 from config import Config
 from db import init_db
-from handlers import start, add
+from handlers import start, add, remove
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,6 +20,7 @@ async def main():
 
     dp.include_router(start.router)
     dp.include_router(add.router)
+    dp.include_router(remove.router)
 
     try:
         await dp.start_polling(bot)
---
+++ handlers/remove.py
@@ -0,0 +1,27 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+
+from db import get_session
+from tables import User, Task
+
+router = Router()
+
+@router.message(Command("remove"))
+async def remove_task(message: types.Message):
+    try:
+        index = int(message.text.split()[1]) - 1
+    except (IndexError, ValueError):
+        await message.answer("Введён неверный формат или номер задачи.")
+        return
+
+    with get_session() as session:
+        tasks = session.query(Task)\
+                       .filter(Task.user_id == message.from_user.id)\
+                       .order_by(Task.id).all()
+
+        if 0 <= index <= (len(tasks) - 1):
+            await message.answer(f"Задачи: {tasks}")
+            session.delete(tasks[index])
+            await message.answer(f"Задача '{tasks[index].description}' удалена.")
+        else:
+            await message.answer("Такой задачи нет.")

4d4748dce825ddb1b7472be6f22d1a1164ad989e
w88 <krippach147@gmail.com>
2026-02-23 12:03:07 +0500

    Config & dependencies setup

--- config.py
+++ config.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    BOT_TOKEN=os.getenv("BOT_TOKEN")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./main.db")
+    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN is not set in .env file")
